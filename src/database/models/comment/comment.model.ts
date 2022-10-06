726af46bececed86f89041cd960eee620c490b54
Luco97 <renatoluco@gmail.com>
2022-10-05 21:39:21 -0300

    ADD definicion de campos para modelo de comentarios

--- src/database/models/comment/comment.model.ts
+++ src/database/models/comment/comment.model.ts
@@ -1,1 +1,24 @@
-export class CommentModel {}
+import {
+  Entity,
+  Column,
+  CreateDateColumn,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity()
+export class CommentModel {
+  @PrimaryGeneratedColumn({})
+  id: number;
+
+  @Column({
+    length: '1000',
+    type: 'varchar',
+    nullable: false,
+  })
+  comment: string;
+
+  @CreateDateColumn({
+    type: 'timestamp',
+  })
+  created_at: Date;
+}
