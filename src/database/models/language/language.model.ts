9e9939b98f1f35d380f4fbaf7a883e68f1244001
Luco97 <renatoluco@gmail.com>
2022-10-05 21:41:49 -0300

    ADD definicion de campos para modelo de lenguaje

--- src/database/models/language/language.model.ts
+++ src/database/models/language/language.model.ts
@@ -1,1 +1,29 @@
-export class LanguageModel {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class LanguageModel {
+  @PrimaryGeneratedColumn({
+    type: 'int',
+  })
+  id: number;
+
+  @Column({
+    type: 'varchar',
+    length: 30,
+    nullable: false,
+  })
+  language: string;
+
+  @Column({
+    type: 'varchar',
+    length: 30,
+    nullable: false,
+  })
+  country: string;
+
+  @Column({
+    type: 'varchar',
+    nullable: true,
+  })
+  country_flag: string;
+}
