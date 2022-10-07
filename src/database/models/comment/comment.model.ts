850d42b3eb86a942e5caed29ae8d6b55100146ec
Luco97 <renatoluco@gmail.com>
2022-10-06 21:21:26 -0300

    ADD propiedades y relaciones a modelo de comentarios

--- src/database/models/comment/comment.model.ts
+++ src/database/models/comment/comment.model.ts
@@ -1,9 +1,13 @@
 import {
   Entity,
   Column,
+  ManyToOne,
   CreateDateColumn,
   PrimaryGeneratedColumn,
 } from 'typeorm';
+
+import { UserModel } from '../user/user.model';
+import { MangaModel } from '../manga/manga.model';
 
 @Entity()
 export class CommentModel {
@@ -21,4 +25,10 @@ export class CommentModel {
     type: 'timestamp',
   })
   created_at: Date;
+
+  @ManyToOne(() => MangaModel, { nullable: false })
+  manga: MangaModel;
+
+  @ManyToOne(() => UserModel, { nullable: false })
+  user: UserModel;
 }
