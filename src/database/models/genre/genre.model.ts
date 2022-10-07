7a39f846331e1dcce5b1a7806f01f5ba93243123
Luco97 <renatoluco@gmail.com>
2022-10-06 21:35:30 -0300

    ADD propiedades y relacion para modelo de generos

--- src/database/models/genre/genre.model.ts
+++ src/database/models/genre/genre.model.ts
@@ -1,1 +1,15 @@
-export class GenreModel {}
+import { Column, Entity, ManyToMany, PrimaryGeneratedColumn } from 'typeorm';
+
+import { MangaModel } from '../manga/manga.model';
+
+@Entity()
+export class GenreModel {
+  @PrimaryGeneratedColumn('increment') id: number;
+
+  @Column({ type: 'varchar' }) name: string;
+
+  @Column({ type: 'varchar' }) description: string;
+
+  @ManyToMany(() => MangaModel, (manga) => manga.genres)
+  mangas: MangaModel[];
+}
