0af0c3901b1d90ccdfa95bfaa6a9cf44122e796b
Luco97 <renatoluco@gmail.com>
2022-10-06 21:21:56 -0300

    ADD propierades y relaciones a entidad de roles

--- src/database/models/role/role.model.ts
+++ src/database/models/role/role.model.ts
@@ -1,1 +1,19 @@
-export class RoleModel {}
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { UserModel } from '../user/user.model';
+
+@Entity()
+export class RoleModel {
+  @PrimaryGeneratedColumn('uuid', {
+    comment: 'ID de un rol, que este comentario exista en algun lado',
+  })
+  uuid: string;
+
+  @Column({
+    nullable: false,
+    // select: false,
+  })
+  role_name: string;
+
+  @OneToMany(() => UserModel, (user) => user.role)
+  users: UserModel[];
+}
