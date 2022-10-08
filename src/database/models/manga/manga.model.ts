fa5c6578dd2e164c6778646fbf13c7ee11e51549
Luco97 <renatoluco@gmail.com>
2022-10-07 22:10:23 -0300

    ADD propiedades y relaciones para modelo de manga

--- src/database/models/manga/manga.model.ts
+++ src/database/models/manga/manga.model.ts
@@ -1,1 +1,44 @@
-export class MangaModel {}
+import {
+  Column,
+  Entity,
+  JoinTable,
+  ManyToOne,
+  ManyToMany,
+  CreateDateColumn,
+  DeleteDateColumn,
+  UpdateDateColumn,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+import { UserModel } from '../user/user.model';
+import { GenreModel } from '../genre/genre.model';
+import { AuthorModel } from '../author/author.model';
+import { LanguageModel } from '../language/language.model';
+
+@Entity()
+export class MangaModel {
+  @PrimaryGeneratedColumn('increment') id: number;
+
+  @Column({ type: 'varchar' }) title: string;
+
+  @Column({ type: 'int' }) pages: number;
+
+  @CreateDateColumn({ type: 'timestamp' }) created_at: Date;
+
+  @UpdateDateColumn({ type: 'timestamp' }) updated_at: Date;
+
+  @DeleteDateColumn({ type: 'timestamp' }) deleted_at: Date;
+
+  @ManyToOne(() => LanguageModel)
+  language: LanguageModel;
+
+  @ManyToMany(() => AuthorModel)
+  authors: AuthorModel[];
+
+  @ManyToMany(() => GenreModel, (genre) => genre.mangas)
+  @JoinTable()
+  genres: GenreModel[];
+
+  @ManyToMany(() => UserModel, (user) => user.mangas)
+  users: UserModel[];
+}
