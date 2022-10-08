d5e02a9575033040320043ea7ae581dba0d3dc62
Luco97 <renatoluco@gmail.com>
2022-10-07 22:17:29 -0300

    ADD se agregan campos y relaciones para autores, se crea relacion para usuarios (un usuario puede ser artista)

--- src/database/models/author/author.model.ts
+++ src/database/models/author/author.model.ts
@@ -1,1 +1,33 @@
-export class AuthorModel {}
+import {
+  Entity,
+  Column,
+  OneToOne,
+  ManyToMany,
+  CreateDateColumn,
+  UpdateDateColumn,
+  DeleteDateColumn,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+import { UserModel } from '../user/user.model';
+import { MangaModel } from '../manga/manga.model';
+@Entity()
+export class AuthorModel {
+  @PrimaryGeneratedColumn('increment') id: number;
+
+  @Column({ type: 'varchar' }) alias: string;
+
+  @Column({ type: 'varchar', length: '1500' }) description: string;
+
+  @CreateDateColumn({ type: 'timestamp' }) create_at: Date;
+
+  @UpdateDateColumn({ type: 'timestamp' }) update_at: Date;
+
+  @DeleteDateColumn({ type: 'timestamp' }) delete_at: Date;
+
+  @OneToOne(() => UserModel, (user) => user.author_profile)
+  user: UserModel;
+
+  @ManyToMany(() => MangaModel)
+  mangas: MangaModel[];
+}
