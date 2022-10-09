5edc2308cab26690c00ccc228bc542a69c76e140
Luco97 <renatoluco@gmail.com>
2022-10-09 19:48:25 -0300

    ADD configuracion para JWT y uso de dotEnv

--- src/shared/auth/auth.module.ts
+++ src/shared/auth/auth.module.ts
@@ -1,7 +1,20 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { AuthService } from './service/auth.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  providers: [AuthService]
+  imports: [
+    ConfigModule.forRoot(),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get('SECRET_KEY'),
+        signOptions: { expiresIn: configService.get('TOKEN_EXPIRATION') },
+      }),
+    }),
+  ],
+  providers: [AuthService],
 })
 export class AuthModule {}
