a02fb39c207caab735e9db9987af7b7e22cf2f45
Luco97 <renatoluco@gmail.com>
2022-10-10 03:02:57 -0300

    ADD se agregan modelos para uso o inject de repositorios para cada entidad

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,6 +1,17 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+// Models or Entities
+import { RoleModel } from './models/role/role.model';
+import { UserModel } from './models/user/user.model';
+import { MangaModel } from './models/manga/manga.model';
+import { GenreModel } from './models/genre/genre.model';
+import { AuthorModel } from './models/author/author.model';
+import { InviteModel } from './models/invite/invite.model';
+import { CommentModel } from './models/comment/comment.model';
+import { LanguageModel } from './models/language/language.model';
+import { EmailVerifyModel } from './models/email-verify/email-verify.model';
+
 // Services
 import { RoleModelService } from './models/role/role.model.service';
 import { UserModelService } from './models/user/user-model.service';
@@ -13,7 +24,22 @@ import { LanguageModelService } from './models/language/language-model.service';
 import { EmailVerifyModelService } from './models/email-verify/email-verify-model.service';
 
 @Module({
-  imports: [TypeOrmModule.forRootAsync({})],
+  imports: [
+    // DB config
+    TypeOrmModule.forRootAsync({}),
+    // repos
+    TypeOrmModule.forFeature([
+      RoleModel,
+      UserModel,
+      MangaModel,
+      GenreModel,
+      AuthorModel,
+      InviteModel,
+      CommentModel,
+      LanguageModel,
+      EmailVerifyModel,
+    ]),
+  ],
   providers: [
     RoleModelService,
     UserModelService,
