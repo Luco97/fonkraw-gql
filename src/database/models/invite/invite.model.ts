6fb65856f0bd74b3138873bcb5fe0489a4eb5fdb
Luco97 <renatoluco@gmail.com>
2022-10-16 01:48:09 -0300

    ADD propiedades para modelo de invitacion entre autores para manga

--- src/database/models/invite/invite.model.ts
+++ src/database/models/invite/invite.model.ts
@@ -1,4 +1,37 @@
-import { Entity } from 'typeorm';
+import {
+  Column,
+  Entity,
+  OneToOne,
+  ManyToOne,
+  JoinColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { AuthorModel } from '../author/author.model';
 
 @Entity()
-export class InviteModel {}
+export class InviteModel {
+  // id de siempre
+  // estado de invitacion
+  // from author (autor A)
+  // to author (autor B) A != B
+  // comentario
+  @PrimaryGeneratedColumn('increment') id: number;
+
+  @Column({ type: 'boolean', default: false }) status: boolean;
+
+  @Column({ type: 'varchar', length: '300', nullable: true }) comment: string;
+
+  @CreateDateColumn({ type: 'timestamp' })
+  created_at: Date;
+
+  @UpdateDateColumn({ type: 'timestamp' }) updated_at: Date;
+
+  @OneToOne(() => AuthorModel)
+  @JoinColumn()
+  from_author: AuthorModel;
+
+  @ManyToOne(() => AuthorModel)
+  to_author: AuthorModel;
+}
