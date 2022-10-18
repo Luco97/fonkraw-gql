47b9235f683d8d05dae6e525c9474c96c3af8db7
Luco97 <renatoluco@gmail.com>
2022-10-18 19:51:29 -0300

    CREATE modulo y servicio de nodemailer

---
+++ src/shared/mail/service/mail.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@nestjs/common';
+// const nodemailer = require('nodemailer');
+import { Transporter, createTransport } from 'nodemailer';
+
+@Injectable()
+export class MailService {
+  private transport: Transporter;
+  constructor() {
+    this.transport = createTransport({
+      service: process.env.MAIL_SERVICE,
+      auth: {
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASS,
+      },
+    });
+  }
+
+  sendMail(email: string, subject: string, html: string) {
+    this.transport.sendMail({
+      from: process.env.MAIL_USER,
+      to: email,
+      subject,
+      html: html,
+    });
+  }
+}
