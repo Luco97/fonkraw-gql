fdf4505b61d92feb8d746a98aa93df49fabd2c03
Luco97 <renatoluco@gmail.com>
2022-10-24 20:05:11 -0300

    ADD logica para confirmar que usuario es valido segun token

--- src/guards/auth.guard.ts
+++ src/guards/auth.guard.ts
@@ -1,11 +1,17 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+
+import { Request } from 'express';
 import { Observable } from 'rxjs';
+
+import { AuthService } from '@shared/auth';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
+  constructor(private _authService: AuthService) {}
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
-    return true;
+    const req: Request = context.switchToHttp().getNext().req;
+    return this._authService.validateToken(req.headers.authorization);
   }
 }
