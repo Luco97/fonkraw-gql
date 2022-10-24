4c10138b41c7140ed684a279b0a916fd2071c79a
Luco97 <renatoluco@gmail.com>
2022-10-24 20:56:21 -0300

    ADD proceso para guardia que valida por conjunto de roles

--- src/guards/role.guard.ts
+++ src/guards/role.guard.ts
@@ -1,11 +1,38 @@
+import { Reflector } from '@nestjs/core';
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
-import { Observable } from 'rxjs';
+
+import { Request } from 'express';
+
+import { AuthService } from '@shared/auth';
+import { RoleModelService } from '@database/models/role';
 
 @Injectable()
 export class RoleGuard implements CanActivate {
-  canActivate(
-    context: ExecutionContext,
-  ): boolean | Promise<boolean> | Observable<boolean> {
-    return true;
+  constructor(
+    private _reflector: Reflector,
+    private _authService: AuthService,
+    private _roleModelService: RoleModelService,
+  ) {}
+  canActivate(context: ExecutionContext): boolean | Promise<boolean> {
+    const req: Request = context.switchToHttp().getNext().req;
+    // needs @SetMetadata('roles', ['basic', 'admin'])
+    const roles: string[] = this._reflector.get<string[]>(
+      'roles',
+      context.getHandler(),
+    );
+    const {
+      context: { type, extra },
+    } = this._authService.userObject(req.headers?.authorization);
+    if (!(extra || type)) return false;
+    return new Promise((resolve, reject) =>
+      this._roleModelService
+        .findOne(type)
+        .then(
+          (role) =>
+            roles.includes(role.role_name) ? resolve(true) : resolve(false),
+          (error) => resolve(false),
+        )
+        .catch((error) => resolve(false)),
+    );
   }
 }
