77a4ac5034de20e89c4e72a20848b93bce9deac6
Luco97 <renatoluco@gmail.com>
2022-10-24 20:56:48 -0300

    ADD inyeccion de reposiotiro y metodo para obtener un rol

--- src/database/models/role/role.model.service.ts
+++ src/database/models/role/role.model.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { RoleModel } from './role.model';
 
 @Injectable()
-export class RoleModelService {}
+export class RoleModelService {
+  constructor(
+    @InjectRepository(RoleModel) private _roleRepo: Repository<RoleModel>,
+  ) {}
+
+  findOne(uuid: string): Promise<RoleModel> {
+    return this._roleRepo
+      .createQueryBuilder('role')
+      .where('role.id = :uuid', { uuid })
+      .getOne();
+  }
+}
