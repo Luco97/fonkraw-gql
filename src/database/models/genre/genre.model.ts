f1634eeb224bcf72dc629b9f1370de109bf88ad3
Luco97 <renatoluco@gmail.com>
2022-10-26 20:56:06 -0300

    ADD campos y tipos para gql de genero

--- src/database/models/genre/genre.model.ts
+++ src/database/models/genre/genre.model.ts
@@ -1,14 +1,23 @@
+import { Field, ObjectType } from '@nestjs/graphql';
+
 import { Column, Entity, ManyToMany, PrimaryGeneratedColumn } from 'typeorm';
 
 import { MangaModel } from '../manga/manga.model';
 
 @Entity()
+@ObjectType()
 export class GenreModel {
-  @PrimaryGeneratedColumn('increment') id: number;
+  @Field(() => Number)
+  @PrimaryGeneratedColumn('increment')
+  id: number;
 
-  @Column({ type: 'varchar' }) name: string;
+  @Field(() => String)
+  @Column({ type: 'varchar' })
+  name: string;
 
-  @Column({ type: 'varchar', nullable: true }) description: string;
+  @Field(() => String)
+  @Column({ type: 'varchar', nullable: true })
+  description: string;
 
   @ManyToMany(() => MangaModel, (manga) => manga.genres)
   mangas: MangaModel[];
