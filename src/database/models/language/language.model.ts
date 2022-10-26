04d0621035c96ad59bfd9c70768881a0110bd1eb
Luco97 <renatoluco@gmail.com>
2022-10-26 20:55:41 -0300

    ADD campos y tipos para gql de idioma

--- src/database/models/language/language.model.ts
+++ src/database/models/language/language.model.ts
@@ -1,14 +1,17 @@
+import { Field, ObjectType } from '@nestjs/graphql';
+
 import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 
 import { MangaModel } from '../manga/manga.model';
 
 @Entity()
+@ObjectType()
 export class LanguageModel {
-  @PrimaryGeneratedColumn({
-    type: 'int',
-  })
+  @Field(() => Number)
+  @PrimaryGeneratedColumn('increment')
   id: number;
 
+  @Field(() => String)
   @Column({
     type: 'varchar',
     length: 30,
@@ -16,6 +19,7 @@ export class LanguageModel {
   })
   language: string;
 
+  @Field(() => String)
   @Column({
     type: 'varchar',
     length: 30,
@@ -23,6 +27,7 @@ export class LanguageModel {
   })
   country: string;
 
+  @Field(() => String)
   @Column({
     type: 'varchar',
     nullable: true,
