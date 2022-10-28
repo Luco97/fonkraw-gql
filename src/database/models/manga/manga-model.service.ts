7c1d2c11b9e6e1ba12068ac6c44b170bbc5815c7
Luco97 <renatoluco@gmail.com>
2022-10-28 00:57:03 -0300

    ADD metodo para encontrar todos los mangas (version base)

--- src/database/models/manga/manga-model.service.ts
+++ src/database/models/manga/manga-model.service.ts
@@ -1,4 +1,36 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { MangaModel } from './manga.model';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class MangaModelService {}
+export class MangaModelService {
+  constructor(
+    @InjectRepository(MangaModel) private _mangaRepo: Repository<MangaModel>,
+  ) {}
+
+  find_all(parameters: {}): Promise<[MangaModel[], number]> {
+    const {} = parameters;
+    return this._mangaRepo
+      .createQueryBuilder('manga')
+      .leftJoinAndSelect('manga.genres', 'genres')
+      .leftJoinAndSelect('manga.authors', 'authors')
+      .leftJoinAndSelect('manga.language', 'language')
+      .loadRelationCountAndMap(
+        'manga.favorites_user',
+        'manga.users',
+        'favorites',
+        (qb) => qb.orderBy('cnt'),
+      )
+      .loadRelationCountAndMap(
+        'manga.commentaries',
+        'manga.comments',
+        'commentarious',
+        (qb) => qb.orderBy('cnt'),
+      )
+      .orderBy()
+      .take()
+      .skip()
+      .getManyAndCount();
+  }
+}
