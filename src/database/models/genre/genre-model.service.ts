98972153452717db91b72555f89cee040a1532ed
Luco97 <renatoluco@gmail.com>
2022-10-28 00:58:36 -0300

    ADD inyeccion de repo, find_all y find_one

--- src/database/models/genre/genre-model.service.ts
+++ src/database/models/genre/genre-model.service.ts
@@ -1,4 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { GenreModel } from './genre.model';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class GenreModelService {}
+export class GenreModelService {
+  constructor(
+    @InjectRepository(GenreModel) private _genreRepo: Repository<GenreModel>,
+  ) {}
+
+  find_all(): Promise<[GenreModel[], number]> {
+    return this._genreRepo
+      .createQueryBuilder('genre')
+      .loadRelationCountAndMap('genre.count', 'genre.mangas', 'mango', (qb) =>
+        qb.orderBy('cnt'),
+      )
+      .getManyAndCount();
+  }
+
+  find_one(id: number): Promise<GenreModel> {
+    return this._genreRepo
+      .createQueryBuilder('genre')
+      .where('genre.id = :id', { id })
+      .loadRelationCountAndMap('genre.count', 'genre.mangas', 'mango', (qb) =>
+        qb.orderBy('cnt'),
+      )
+      .getOne();
+  }
+}
