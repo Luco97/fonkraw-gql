79ceb449ba1ebc15791384412645998d249a254b
Luco97 <renatoluco@gmail.com>
2022-10-31 02:44:02 -0300

    ADD metodo para saber si es creador de manga y metodo para validar si puede enviar una invitacion a manga si es propio del usuario

--- src/database/models/user/user-model.service.ts
+++ src/database/models/user/user-model.service.ts
@@ -44,4 +44,29 @@ export class UserModelService {
       .where('user.email =: email', { email })
       .getOne();
   }
+
+  // Manga features
+  manga_creator(parameters: { user_id: number; manga_id: number }) {
+    const { user_id, manga_id } = parameters;
+    return this._userRepo
+      .createQueryBuilder('user')
+      .leftJoin('user.author_profile', 'author_profile')
+      .leftJoin('author_profile.init_mangas', 'init_mangas')
+      .where('init_mangas.id = :manga_id', { manga_id })
+      .andWhere('user.id = :user_id', { user_id })
+      .getCount();
+  }
+
+  // Invite feature
+  // Valid user to send invitation, if return value > 0
+  invite_creator(parameters: { user_id: number; manga_id: number }) {
+    const { manga_id, user_id } = parameters;
+    return this._userRepo
+      .createQueryBuilder('user')
+      .leftJoin('user.author_profile', 'author_profile')
+      .leftJoin('author_profile.init_mangas', 'init_mangas')
+      .where('init_mangas.id = :manga_id', { manga_id })
+      .andWhere('user.id = :user_id', { user_id })
+      .getCount();
+  }
 }
