db0fd905c4cdd8a8168afee8c0f1c4dbfa57999e
Luco97 <renatoluco@gmail.com>
2022-11-01 03:15:10 -0300

    ADD inyeccion de repo y creacion de metodos para verificar email

--- src/database/models/email-verify/email-verify-model.service.ts
+++ src/database/models/email-verify/email-verify-model.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+
+import { Repository } from 'typeorm';
+
+import { EmailVerifyModel } from './email-verify.model';
 
 @Injectable()
-export class EmailVerifyModelService {}
+export class EmailVerifyModelService {
+  constructor(
+    @InjectRepository(EmailVerifyModel)
+    private _emailRepo: Repository<EmailVerifyModel>,
+  ) {}
+  get create(): Promise<EmailVerifyModel> {
+    return this._emailRepo.save(this._emailRepo.create());
+  }
+
+  find_one(uuid: string): Promise<EmailVerifyModel> {
+    return this._emailRepo
+      .createQueryBuilder('email')
+      .where('email.uuid = :uuid', { uuid })
+      .getOne();
+  }
+
+  update(uuid: string): Promise<EmailVerifyModel> {
+    return this._emailRepo.save({ uuid, activated: true });
+  }
+}
