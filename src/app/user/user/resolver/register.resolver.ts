5c8e7e5ae07d18c73af06bb95748544fc0d35d22
Luco97 <renatoluco@gmail.com>
2022-11-02 00:25:33 -0300

    ADD metodo para creacion de usuario

--- src/app/user/user/resolver/register.resolver.ts
+++ src/app/user/user/resolver/register.resolver.ts
@@ -1,4 +1,18 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
+
+import { UserService } from '../services/user.service';
+import { RegisterInput } from '../inputs/register.input';
+import { RegisterOutput } from '../outputs/register.output';
 
 @Resolver()
-export class RegisterResolver {}
+export class RegisterResolver {
+  constructor(private _userService: UserService) {}
+
+  @Mutation(() => RegisterOutput)
+  register_user(
+    @Args('user') create_user: RegisterInput,
+  ): Promise<RegisterOutput> {
+    const { email, password, username } = create_user;
+    return this._userService.create_user({ email, username, password });
+  }
+}
