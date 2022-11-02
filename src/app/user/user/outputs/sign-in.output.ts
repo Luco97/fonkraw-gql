96ba7f2cf03fe748117d1724da116176f98eea96
Luco97 <renatoluco@gmail.com>
2022-11-02 00:26:16 -0300

    UPDATE token es opcional

--- src/app/user/user/outputs/sign-in.output.ts
+++ src/app/user/user/outputs/sign-in.output.ts
@@ -5,5 +5,5 @@ import { RegisterOutput } from './register.output';
 @ObjectType({ description: 'output object in sign in mutation' })
 export class SignInOutput extends RegisterOutput {
   @Field(() => String, { nullable: true, defaultValue: '' })
-  token: string;
+  token?: string;
 }
