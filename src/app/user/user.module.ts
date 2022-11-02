df14dc6c43a720e6cae40b05f73031d67af23a10
Luco97 <renatoluco@gmail.com>
2022-11-02 00:22:04 -0300

    ADD servicios, resolvers y modulosp para gestion de usuario

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,21 +1,28 @@
 import { Module } from '@nestjs/common';
 
+// Modules
+import { AuthModule } from '@shared/auth';
 import { UserModelModule } from '@database/models/user';
 import { RoleModelModule } from '@database/models/role';
 import { InviteModelModule } from '@database/models/invite';
 import { EmailVerifyModelModule } from '@database/models/email-verify';
 
+// Resolvers
 import { ReadResolver } from './invite/resolver/read.resolver';
 import { SignInResolver } from './user/resolver/sign-in.resolver';
 import { RegisterResolver } from './user/resolver/register.resolver';
 
+// Services
+import { UserService } from './user/services/user.service';
+
 @Module({
   imports: [
+    AuthModule,
     UserModelModule,
     RoleModelModule,
     InviteModelModule,
     EmailVerifyModelModule,
   ],
-  providers: [SignInResolver, RegisterResolver, ReadResolver],
+  providers: [SignInResolver, RegisterResolver, ReadResolver, UserService],
 })
 export class UserModule {}
