2d451988475f50995f7fa0f21f71a6c4bea56279
Luco97 <renatoluco@gmail.com>
2022-11-02 00:25:58 -0300

    ADD metodo para funcionalidad de identificacion de usuario

--- src/app/user/user/resolver/sign-in.resolver.ts
+++ src/app/user/user/resolver/sign-in.resolver.ts
@@ -1,4 +1,17 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
+
+import { SignInInput } from '../inputs/sign-in.input';
+import { UserService } from '../services/user.service';
+import { SignInOutput } from '../outputs/sign-in.output';
 
 @Resolver()
-export class SignInResolver {}
+export class SignInResolver {
+  constructor(private _userService: UserService) {}
+  @Mutation(() => SignInOutput)
+  sign_in(
+    @Args('credentials') user_credential: SignInInput,
+  ): Promise<SignInOutput> {
+    const { email, password } = user_credential;
+    return this._userService.sign_in_user({ email, password });
+  }
+}
