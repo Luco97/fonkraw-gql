67bc71e178bc3e9cb52feb569288f663533e3e2f
Luco97 <renatoluco@gmail.com>
2022-11-05 03:00:44 -0300

    ADD modulo para enviar mail de confirmacion

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 // Modules
 import { AuthModule } from '@shared/auth';
+import { MailModule } from '@shared/mail';
 import { UserModelModule } from '@database/models/user';
 import { RoleModelModule } from '@database/models/role';
 import { InviteModelModule } from '@database/models/invite';
@@ -17,11 +19,13 @@ import { UserService } from './user/services/user.service';
 
 @Module({
   imports: [
+    MailModule,
     AuthModule,
     UserModelModule,
     RoleModelModule,
     InviteModelModule,
     EmailVerifyModelModule,
+    ConfigModule.forRoot({}),
   ],
   providers: [SignInResolver, RegisterResolver, ReadResolver, UserService],
 })
