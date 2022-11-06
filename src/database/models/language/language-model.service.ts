13fdb2fdd2b67a1ae3fc5ec3b38ec4067a2e74f2
Luco97 <renatoluco@gmail.com>
2022-11-05 21:15:38 -0300

    ADD repository de idioma

--- src/database/models/language/language-model.service.ts
+++ src/database/models/language/language-model.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+
+import { Repository } from 'typeorm';
+
+import { LanguageModel } from './language.model';
 
 @Injectable()
-export class LanguageModelService {}
+export class LanguageModelService {
+  constructor(
+    @InjectRepository(LanguageModel)
+    private _languageRepo: Repository<LanguageModel>,
+  ) {}
+}
