5d950196aaa6628cd9a60e4defef970323c98a9d
Luco97 <renatoluco@gmail.com>
2022-11-07 00:10:34 -0300

    ADD modulos para resolvers y resolvers para autor

--- src/app/fan-work/author/author.module.ts
+++ src/app/fan-work/author/author.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
-import { ReadResolver } from './resolver/read.resolver';
+
+import { AuthModule } from '@shared/auth';
+import { UserModelModule } from '@database/models/user';
+import { AuthorModelModule } from '@database/models/author';
+
+import { ReadResolver } from './resolvers/read.resolver';
+import { CreateResolver } from './resolvers/create.resolver';
+import { UpdateResolver } from './resolvers/update.resolver';
 
 @Module({
-  providers: [ReadResolver],
+  imports: [AuthorModelModule, UserModelModule, AuthModule],
+  providers: [CreateResolver, UpdateResolver, ReadResolver],
 })
 export class AuthorModule {}
