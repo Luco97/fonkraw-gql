2e3bbe03db64762781868822cc538318cae0eda9
Luco97 <renatoluco@gmail.com>
2022-11-11 22:43:41 -0300

    ADD metodo para crear autor asociado a un usario

--- src/app/fan-work/author/resolvers/create.resolver.ts
+++ src/app/fan-work/author/resolvers/create.resolver.ts
@@ -1,3 +1,4 @@
+import { UseGuards, HttpStatus } from '@nestjs/common';
 import { Args, Context, Mutation, Resolver } from '@nestjs/graphql';
 
 import { Request } from 'express';
@@ -6,6 +7,8 @@ import { AuthService } from '@shared/auth';
 import { CreateInput } from '../inputs/create.input';
 import { UserModelService } from '@database/models/user';
 import { AuthorModel, AuthorModelService } from '@database/models/author';
+import { AuthGuard } from '@guard/auth.guard';
+import { CreateOutput } from '../outputs/create.output';
 
 @Resolver()
 export class CreateResolver {
@@ -15,15 +18,37 @@ export class CreateResolver {
     private _authorModel: AuthorModelService,
   ) {}
 
-  @Mutation(() => AuthorModel)
+  @Mutation(() => CreateOutput)
+  @UseGuards(AuthGuard)
   create(
     @Args('author', { nullable: true }) createInput: CreateInput,
     @Context() context,
-  ) {
+  ): Promise<CreateOutput> {
     const req: Request = context.req;
     const token: string = req.headers?.authorization;
+    
+    const { alias, description } = createInput;
+    const user_id: number = this._authService.userID(token);
 
-    const {} = createInput;
-    const id_user: number = this._authService.userID(token);
+    return new Promise<CreateOutput>((resolve, reject) => {
+      this._authorModel.author_check({ user_id, alias }).then((author) => {
+        if (author)
+          resolve({
+            author,
+            message: 'author already exist',
+            status: HttpStatus.AMBIGUOUS,
+          });
+        else
+          this._authorModel
+            .create({ alias, description, user_id })
+            .then((new_author) =>
+              resolve({
+                author: new_author,
+                message: `Happy to see you, ${new_author.alias}`,
+                status: HttpStatus.OK,
+              }),
+            );
+      });
+    });
   }
 }
