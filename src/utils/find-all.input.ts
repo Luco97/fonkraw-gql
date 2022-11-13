9702e929c03cccdab91ed2fe3e4799596c49ac16
Luco97 <renatoluco@gmail.com>
2022-11-13 03:03:53 -0300

    CREATE input para definir propiedades tipicas de un find-all de una entidad

---
+++ src/utils/find-all.input.ts
@@ -0,0 +1,33 @@
+import { Field, InputType } from '@nestjs/graphql';
+
+@InputType()
+// @ArgsType() // No funciona como dice la documentacion, seguir usando InputType
+export class findAll {
+  @Field(() => Number, {
+    nullable: true,
+    defaultValue: 10,
+    description: 'quantity of items',
+  })
+  take: number;
+
+  @Field(() => Number, {
+    nullable: true,
+    defaultValue: 0,
+    description: 'number of items to skip',
+  })
+  skip: number;
+
+  @Field(() => String, {
+    nullable: true,
+    defaultValue: 'create_at',
+    description: 'field selected for order',
+  })
+  orderBy: string;
+
+  @Field(() => String, {
+    nullable: true,
+    defaultValue: 'ASC',
+    description: 'if is ASCendent or DESCendent',
+  })
+  order: 'ASC' | 'DESC';
+}
