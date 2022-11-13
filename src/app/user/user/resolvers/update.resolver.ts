c02e00e6631bf45c3d4b6efaee39fc194926081e
Luco97 <renatoluco@gmail.com>
2022-11-13 03:17:37 -0300

    ADD resolvers para camibar password

--- src/app/user/user/resolvers/update.resolver.ts
+++ src/app/user/user/resolvers/update.resolver.ts
@@ -8,6 +8,8 @@ import { AuthGuard } from '@guard/auth.guard';
 import { UpdateInput } from '../inputs/update.input';
 import { UserService } from '../services/user.service';
 import { UpdateOutput } from '../outputs/update.output';
+import { RegisterOutput } from '../outputs/register.output';
+import { UpdatePasswordInput } from '../inputs/update-password.input';
 
 @Resolver()
 export class UpdateResolver {
@@ -29,5 +31,38 @@ export class UpdateResolver {
     const user_id: number = this._authService.userID(token);
 
     return this._userService.update_info({ description, image_url, user_id });
+  }
+
+  // pass change
+  // First step (guard guest)
+  @Mutation(() => Boolean)
+  @UseGuards(AuthGuard)
+  confirm_password(
+    @Args('update_pass') updatePass: UpdatePasswordInput,
+    @Context() context,
+  ): Promise<boolean> {
+    const req: Request = context.req;
+    const token: string = req.headers?.authorization;
+
+    const { password } = updatePass;
+    const user_id: number = this._authService.userID(token);
+
+    return this._userService.confirm_pass({ user_id, password });
+  }
+
+  // Second step
+  @Mutation(() => RegisterOutput)
+  @UseGuards(AuthGuard)
+  update_password(
+    @Args('update_pass') updatePass: UpdatePasswordInput,
+    @Context() context,
+  ) {
+    const req: Request = context.req;
+    const token: string = req.headers?.authorization;
+
+    const { password } = updatePass;
+    const user_id: number = this._authService.userID(token);
+
+    return this._userService.update_pass({ user_id, password });
   }
 }
