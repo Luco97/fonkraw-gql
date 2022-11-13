db19cf8f31e86d395a6fa67eac7f0b03afb051ec
Luco97 <renatoluco@gmail.com>
2022-11-13 03:15:03 -0300

    ADD descripcion de fields y son opcionales

--- src/utils/response.output.ts
+++ src/utils/response.output.ts
@@ -2,9 +2,17 @@ import { Field, ObjectType } from '@nestjs/graphql';
 
 @ObjectType()
 export class response {
-  @Field(() => String, { nullable: true, defaultValue: 'message not defined' })
+  @Field(() => String, {
+    nullable: true,
+    defaultValue: 'message not defined',
+    description: 'message for extra info',
+  })
   message?: string;
 
-  @Field(() => Number, { nullable: true, defaultValue: 0 })
+  @Field(() => Number, {
+    nullable: true,
+    defaultValue: 0,
+    description: 'status response',
+  })
   status?: Number;
 }
