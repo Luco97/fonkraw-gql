345e47b8010ba8104f01a19e8a8f399e547593d2
Luco97 <renatoluco@gmail.com>
2022-11-15 21:10:55 -0300

    ADD minidescripcion

--- src/app/fan-work/author/inputs/read.input.ts
+++ src/app/fan-work/author/inputs/read.input.ts
@@ -1,7 +1,7 @@
 import { Field, InputType } from '@nestjs/graphql';
 import { findAll } from '@utils/find-all.input';
 
-@InputType('find_all_authors', {})
+@InputType('find_all_authors', { description: 'find all authors options' })
 export class ReadInput extends findAll {
   @Field(() => String, { nullable: true, defaultValue: '' })
   alias?: string;
