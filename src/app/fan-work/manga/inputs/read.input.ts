4637d9e4455b901160f4f5a1fa5c5c9ad08e3887
Luco97 <renatoluco@gmail.com>
2022-11-18 20:44:11 -0300

    UPDATE se aplica extends de findAll input para encontrar editables

--- src/app/fan-work/manga/inputs/read.input.ts
+++ src/app/fan-work/manga/inputs/read.input.ts
@@ -1,14 +1,9 @@
 import { Field, InputType } from '@nestjs/graphql';
+
 import { findAll } from '@utils/find-all.input';
 
 @InputType()
 export class ReadInput extends findAll {}
 
 @InputType()
-export class ReadEditablesInput {
-  @Field(() => Number, { defaultValue: 0 })
-  skip: number;
-
-  @Field(() => Number, { defaultValue: 10 })
-  take: number;
-}
+export class ReadEditablesInput extends findAll {}
