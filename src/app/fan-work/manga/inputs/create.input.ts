0f70a50dbb2d5565a9e898d90426c54134982e75
Luco97 <renatoluco@gmail.com>
2022-11-18 20:46:05 -0300

    ADD validaciones en input para crear manga

--- src/app/fan-work/manga/inputs/create.input.ts
+++ src/app/fan-work/manga/inputs/create.input.ts
@@ -1,13 +1,16 @@
 import { Field, InputType } from '@nestjs/graphql';
+import { IsAlphanumeric, IsUrl } from 'class-validator';
 
 @InputType()
 export class CreateInput {
   @Field(() => String)
+  @IsAlphanumeric()
   title: string;
 
   @Field(() => Number)
   pages: number;
 
   @Field(() => String)
+  @IsUrl()
   cover_url: string;
 }
