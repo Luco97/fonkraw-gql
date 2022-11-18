5f9fa10599fe26cdef3c503aee2b43b0db74db62
Luco97 <renatoluco@gmail.com>
2022-11-18 20:46:55 -0300

    ADD validacion para input de creacion de autor

--- src/app/fan-work/author/inputs/create.input.ts
+++ src/app/fan-work/author/inputs/create.input.ts
@@ -1,8 +1,11 @@
 import { Field, InputType } from '@nestjs/graphql';
+import { IsAlphanumeric, MinLength } from 'class-validator';
 
 @InputType()
 export class CreateInput {
   @Field(() => String)
+  @IsAlphanumeric()
+  @MinLength(3)
   alias: string;
 
   @Field(() => String, { nullable: true, defaultValue: '' })
