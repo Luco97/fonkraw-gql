d7e3bbeb4250376b5290b26d98339508eb934bd7
Luco97 <renatoluco@gmail.com>
2022-11-19 22:01:31 -0300

    ADD resolvers y modulos

--- src/app/fan-work/manga/manga.module.ts
+++ src/app/fan-work/manga/manga.module.ts
@@ -1,7 +1,18 @@
 import { Module } from '@nestjs/common';
-import { ReadResolver } from './resolver/read.resolver';
+
+// Modules
+import { AuthModule } from '@shared/auth';
+import { MangaModelModule } from '@database/models/manga';
+import { AuthorModelModule } from '@database/models/author';
+
+// Resolvers
+import { ReadResolver } from './resolvers/read.resolver';
+import { CreateResolver } from './resolvers/create.resolver';
+import { DeleteResolver } from './resolvers/delete.resolver';
+import { UpdateResolver } from './resolvers/update.resolver';
 
 @Module({
-  providers: [ReadResolver]
+  imports: [AuthModule, MangaModelModule, AuthorModelModule],
+  providers: [ReadResolver, CreateResolver, DeleteResolver, UpdateResolver],
 })
 export class MangaModule {}
