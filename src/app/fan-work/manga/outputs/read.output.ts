defaa57a3e33b342b079ca4ebcbb1eef249ddcde
Luco97 <renatoluco@gmail.com>
2022-11-20 02:14:15 -0300

    ADD output para find_all de mangas

--- src/app/fan-work/manga/outputs/read.output.ts
+++ src/app/fan-work/manga/outputs/read.output.ts
@@ -1,1 +1,12 @@
-export class ReadOutput {}
+import { MangaModel } from '@database/models/manga';
+import { Field, ObjectType } from '@nestjs/graphql';
+import { response } from '@utils/response.output';
+
+@ObjectType('read_all_manga')
+export class ReadAllOutput extends response {
+  @Field(() => [MangaModel], { defaultValue: [] })
+  mangas: MangaModel[];
+
+  @Field(() => Number)
+  count: number;
+}
