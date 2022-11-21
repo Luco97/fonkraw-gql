460bc910bdc143c3901579e58648a8bea37a0954
Luco97 <renatoluco@gmail.com>
2022-11-21 20:42:53 -0300

    ADD resolver para buscar todos los mangas

--- src/app/fan-work/manga/resolvers/read.resolver.ts
+++ src/app/fan-work/manga/resolvers/read.resolver.ts
@@ -1,4 +1,57 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Context, Query, Resolver } from '@nestjs/graphql';
+
+import { Request } from 'express';
+
+import { AuthService } from '@shared/auth';
+import { find_all_default } from '@utils/find-all.input';
+import { MangaModelService } from '@database/models/manga';
+
+import { ReadInput } from '../inputs/read.input';
+import { ReadAllOutput } from '../outputs/read.output';
+import { HttpStatus } from '@nestjs/common';
 
 @Resolver()
-export class ReadResolver {}
+export class ReadResolver {
+  constructor(
+    private _authService: AuthService,
+    private _mangaModel: MangaModelService,
+  ) {}
+  @Query(() => ReadAllOutput, { name: 'find_all_manga' })
+  find_all(
+    @Args('options', {
+      nullable: true,
+      defaultValue: { search: '', ...find_all_default },
+    })
+    readInput: ReadInput,
+    @Context() context,
+  ): Promise<ReadAllOutput> {
+    const req: Request = context.req;
+    const token: string = req.headers?.authorization;
+    const user_id: number = this._authService.userID(token);
+
+    const { order, orderBy, skip, take, search } = readInput || {
+      search: '',
+      ...find_all_default,
+    };
+
+    return new Promise<ReadAllOutput>((resolve, reject) =>
+      this._mangaModel
+        .find_all({
+          skip,
+          take,
+          order,
+          orderProperty: orderBy,
+          search,
+          user_id,
+        })
+        .then(([mangas, count]) =>
+          resolve({
+            count,
+            mangas,
+            message: `total mangas found: ${count}`,
+            status: HttpStatus.OK,
+          }),
+        ),
+    );
+  }
+}
