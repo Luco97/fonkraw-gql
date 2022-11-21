2894b70a5a35f799de3329a32fb008ca50dca02c
Luco97 <renatoluco@gmail.com>
2022-11-21 20:42:17 -0300

    ADD propiedad search propia de buscar en mangas

--- src/app/fan-work/manga/inputs/read.input.ts
+++ src/app/fan-work/manga/inputs/read.input.ts
@@ -3,7 +3,10 @@ import { Field, InputType } from '@nestjs/graphql';
 import { findAll } from '@utils/find-all.input';
 
 @InputType()
-export class ReadInput extends findAll {}
+export class ReadInput extends findAll {
+  @Field(() => String, { nullable: true, defaultValue: '' })
+  search?: string;
+}
 
 @InputType()
 export class ReadEditablesInput extends findAll {}
