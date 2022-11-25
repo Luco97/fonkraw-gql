116bf7b074d93b525c5c00deb4d89fcdff5a9132
Luco97 <renatoluco@gmail.com>
2022-11-24 21:11:16 -0300

    CREATE resolvers para comentarios de manga

--- src/app/fan-work/comment/comment.module.ts
+++ src/app/fan-work/comment/comment.module.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common';
+// Modules
+
+// Resolvers
+import { GetResolver } from './resolvers/get.resolver';
 import { ReadResolver } from './resolver/read.resolver';
+import { CreateResolver } from './resolvers/create.resolver';
+import { DeleteResolver } from './resolvers/delete.resolver';
 
 @Module({
-  providers: [ReadResolver]
+  imports: [],
+  providers: [ReadResolver, CreateResolver, GetResolver, DeleteResolver],
 })
 export class CommentModule {}
