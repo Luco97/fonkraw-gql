de578a8d06d179e14a728fe23848fd8f4a7fc4d2
Luco97 <renatoluco@gmail.com>
2022-11-26 17:24:59 -0300

    CREATE output para obtener todos los comentarios de un manga paginado

--- src/app/fan-work/comment/outputs/read.output.ts
+++ src/app/fan-work/comment/outputs/read.output.ts
@@ -1,1 +1,13 @@
-export class ReadOutput {}
+import { Field, ObjectType } from '@nestjs/graphql';
+
+import { response } from '@utils/response.output';
+import { CommentModel } from '@database/models/comment';
+
+@ObjectType()
+export class ReadAllOutput extends response {
+  @Field(() => [CommentModel])
+  comments: CommentModel[];
+
+  @Field(() => Number)
+  count: number;
+}
