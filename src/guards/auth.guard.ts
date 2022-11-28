ae887dac47245923a1d0efc5166bb83bd550d33b
Luco97 <renatoluco@gmail.com>
2022-11-28 00:49:20 -0300

    UPDATE se quita observable no se utiliza import

--- src/guards/auth.guard.ts
+++ src/guards/auth.guard.ts
@@ -1,7 +1,6 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 
 import { Request } from 'express';
-import { Observable } from 'rxjs';
 
 import { AuthService } from '@shared/auth';
 
