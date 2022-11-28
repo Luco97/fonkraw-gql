e54a79f8571fd01e55c0fa693711ec18078668e6
Luco97 <renatoluco@gmail.com>
2022-11-28 00:48:36 -0300

    CREATE output utilizado para crear manga

--- src/app/fan-work/manga/outputs/create.output.ts
+++ src/app/fan-work/manga/outputs/create.output.ts
@@ -1,1 +1,10 @@
-export class CreateOutput {}
+import { Field, ObjectType } from '@nestjs/graphql';
+
+import { response } from '@utils/response.output';
+import { MangaModel } from '@database/models/manga';
+
+@ObjectType()
+export class CreateOutput extends response {
+  @Field(() => MangaModel, { nullable: true })
+  manga?: MangaModel;
+}
