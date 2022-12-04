d8c9ae8346eaf0eb532b882d777a6e4f33ce0f02
Luco97 <renatoluco@gmail.com>
2022-12-04 01:58:23 -0300

    CREATE input para crear comentario para un manga

--- src/app/fan-work/comment/inputs/create.input.ts
+++ src/app/fan-work/comment/inputs/create.input.ts
@@ -1,1 +1,8 @@
-export class CreateInput {}
+import { Field, InputType } from '@nestjs/graphql';
+
+@InputType('create_comment')
+export class CreateInput {
+  @Field(() => String) comment: string;
+
+  @Field(() => Number) manga_id: number;
+}
