57753386210b75f23fa794458ed10efff6651bdb
Luco97 <renatoluco@gmail.com>
2022-12-04 01:58:43 -0300

    CREATE output para obtener comentarios de un manga

--- src/app/fan-work/comment/outputs/create.output.ts
+++ src/app/fan-work/comment/outputs/create.output.ts
@@ -1,1 +1,9 @@
-export class CreateOutput {}
+import { CommentModel } from '@database/models/comment';
+import { Field, ObjectType } from '@nestjs/graphql';
+import { response } from '@utils/response.output';
+
+@ObjectType()
+export class CreateOutput extends response {
+  @Field(() => CommentModel, { nullable: true })
+  comment?: CommentModel;
+}
