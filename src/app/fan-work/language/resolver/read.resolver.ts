5712efa235bdbc0e0f81aa7917a16a1bd2b2dd76
Luco97 <renatoluco@gmail.com>
2022-12-11 01:23:35 -0300

    ADD metodo para obtener todos los idiomas disponibles

--- src/app/fan-work/language/resolver/read.resolver.ts
+++ src/app/fan-work/language/resolver/read.resolver.ts
@@ -1,4 +1,21 @@
-import { Resolver } from '@nestjs/graphql';
+import { HttpStatus } from '@nestjs/common';
+import { Query, Resolver } from '@nestjs/graphql';
+
+import { LanguageModelService } from '@database/models/language';
+import { ReadOutput } from '../outputs/read.output';
 
 @Resolver()
-export class ReadResolver {}
+export class ReadResolver {
+  constructor(private _languageModel: LanguageModelService) {}
+
+  @Query(() => ReadOutput)
+  find_all_languages(): Promise<ReadOutput> {
+    return new Promise<ReadOutput>((resolve, reject) =>
+      this._languageModel
+        .find_all()
+        .then(([languages, count]) =>
+          resolve({ count, languages, message: '', status: HttpStatus.OK }),
+        ),
+    );
+  }
+}
