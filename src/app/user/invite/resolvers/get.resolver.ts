632199ec5f1f19ee85461d2cc0bab4ead277cbf0
Luco97 <renatoluco@gmail.com>
2022-12-18 19:34:51 -0300

    ADD se crea metodo para obtener todas las invitaciones por usuario

--- src/app/user/invite/resolvers/get.resolver.ts
+++ src/app/user/invite/resolvers/get.resolver.ts
@@ -1,4 +1,26 @@
-import { Resolver } from '@nestjs/graphql';
+import { UseGuards } from '@nestjs/common';
+import { Context, Query, Resolver } from '@nestjs/graphql';
+import { Request } from 'express';
+
+import { AuthService } from '@shared/auth';
+import { AuthGuard } from '@guard/auth.guard';
+import { GetAllOutput } from '../outputs/get-all.output';
+import { InviteService } from '../services/invite.service';
 
 @Resolver()
-export class GetResolver {}
+export class GetResolver {
+  constructor(
+    private _authService: AuthService,
+    private _inviteService: InviteService,
+  ) {}
+
+  @Query(() => GetAllOutput)
+  @UseGuards(AuthGuard)
+  get_all_invites(@Context() context): Promise<GetAllOutput> {
+    const req: Request = context.req;
+    const token: string = req.headers?.authorization;
+    const user_id: number = this._authService.userID(token);
+
+    return this._inviteService.get_all_invites(user_id);
+  }
+}
