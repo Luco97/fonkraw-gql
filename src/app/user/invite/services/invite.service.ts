b6312ab9193eb6def37877b7615a937d46529858
Luco97 <renatoluco@gmail.com>
2022-12-19 23:07:32 -0300

    ADD metodo para obtener todas las invitaciones de un manga

--- src/app/user/invite/services/invite.service.ts
+++ src/app/user/invite/services/invite.service.ts
@@ -1,4 +1,5 @@
 import { Injectable, HttpStatus } from '@nestjs/common';
+
 import { InviteModelService } from '@database/models/invite';
 import { GetAllOutput } from '../outputs/get-all.output';
 
@@ -16,6 +17,25 @@ export class InviteService {
           status: HttpStatus.OK,
         }),
       ),
+    );
+  }
+
+  all_invite_manga(parameters: {
+    manga_id: number;
+    user_id: number;
+  }): Promise<GetAllOutput> {
+    const { manga_id, user_id } = parameters;
+    return new Promise<GetAllOutput>((resolver, reject) =>
+      this._inviteModel
+        .find_all_from_manga({ manga_id, user_id })
+        .then(([invites, count]) =>
+          resolver({
+            invites,
+            count,
+            message: `authors invites found: ${count}`,
+            status: HttpStatus.OK,
+          }),
+        ),
     );
   }
 }
