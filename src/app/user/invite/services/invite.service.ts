afc9e5b4a49d2acb6bf73b1be6c9571d1e6444d9
Luco97 <renatoluco@gmail.com>
2022-12-20 20:53:31 -0300

    ADD metodo para crear invitacion

--- src/app/user/invite/services/invite.service.ts
+++ src/app/user/invite/services/invite.service.ts
@@ -1,11 +1,16 @@
 import { Injectable, HttpStatus } from '@nestjs/common';
 
+import { UserModelService } from '@database/models/user';
 import { InviteModelService } from '@database/models/invite';
 import { GetAllOutput } from '../outputs/get-all.output';
+import { CreateInviteOutput } from '../outputs/create-invite.output';
 
 @Injectable()
 export class InviteService {
-  constructor(private _inviteModel: InviteModelService) {}
+  constructor(
+    private _userService: UserModelService,
+    private _inviteModel: InviteModelService,
+  ) {}
 
   get_all_invites(user_id: number): Promise<GetAllOutput> {
     return new Promise<GetAllOutput>((resolve, reject) =>
@@ -37,5 +42,30 @@ export class InviteService {
           }),
         ),
     );
+  }
+
+  create_invite(parameters: {
+    manga_id: number;
+    user_id: number;
+    to_author_id: number;
+  }) {
+    const { user_id, manga_id, to_author_id } = parameters;
+    return new Promise<CreateInviteOutput>((resolver, reject) => {
+      Promise.all([
+        this._userService.author_check({ author_id: to_author_id }), // check if this author exist
+        this._userService.manga_creator({ user_id, manga_id }), // check if the user is creator of this manga
+        this._inviteModel.find_invite_origin({
+          manga_id,
+          to_author_id,
+          user_id,
+        }), // check if already exist one invite register in DB (in the manga_id)
+      ]).then(([author_exist, is_valid, valid_invite]) => {
+        if (!(author_exist?.id && is_valid && valid_invite?.id)) resolver({});
+        else {
+          // author_exist.email send email of invite
+          // emit with socket notification
+        }
+      });
+    });
   }
 }
