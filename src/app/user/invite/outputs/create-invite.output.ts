6e1bf783881a889c3fb01743bf8884255eda5e10
Luco97 <renatoluco@gmail.com>
2022-12-21 19:19:12 -0300

    ADD se agrega campo para invitacion

--- src/app/user/invite/outputs/create-invite.output.ts
+++ src/app/user/invite/outputs/create-invite.output.ts
@@ -1,5 +1,10 @@
-import { ObjectType } from '@nestjs/graphql';
+import { Field, ObjectType } from '@nestjs/graphql';
+
 import { response } from '@utils/response.output';
+import { InviteModel } from '@database/models/invite';
 
 @ObjectType()
-export class CreateInviteOutput extends response {}
+export class CreateInviteOutput extends response {
+  @Field(() => InviteModel, { nullable: true })
+  invite?: InviteModel;
+}
