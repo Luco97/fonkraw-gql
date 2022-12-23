da18d4f9bb187c9e595321629b2c81a5a42eb08e
Luco97 <renatoluco@gmail.com>
2022-12-23 20:03:47 -0300

    ADD uso de servicio y guardia para crear invitacion

--- src/app/user/invite/resolvers/create.resolver.ts
+++ src/app/user/invite/resolvers/create.resolver.ts
@@ -1,4 +1,38 @@
-import { Resolver } from '@nestjs/graphql';
+import { UseGuards } from '@nestjs/common';
+import { Args, Context, Mutation, Resolver } from '@nestjs/graphql';
+
+import { Request } from 'express';
+
+import { AuthService } from '@shared/auth';
+import { AuthGuard } from '@guard/auth.guard';
+import { InviteService } from '../services/invite.service';
+import { CreateInviteInput } from '../inputs/create-invite.input';
+import { CreateInviteOutput } from '../outputs/create-invite.output';
 
 @Resolver()
-export class CreateResolver {}
+export class CreateResolver {
+  constructor(
+    private _authService: AuthService,
+    private _inviteService: InviteService,
+  ) {}
+
+  @Mutation(() => CreateInviteOutput)
+  @UseGuards(AuthGuard)
+  create_invite(
+    @Args('parameters') create_input: CreateInviteInput,
+    @Context() context,
+  ) {
+    const req: Request = context.req;
+    const token: string = req.headers?.authorization;
+    const user_id: number = this._authService.userID(token);
+
+    const { comment, manga_id, to_author_id } = create_input;
+
+    return this._inviteService.create_invite({
+      comment,
+      manga_id,
+      user_id,
+      to_author_id,
+    });
+  }
+}
