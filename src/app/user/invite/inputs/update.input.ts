0f1bc070d5264f9cdf9a3edbd58f425794eea3ce
Luco97 <renatoluco@gmail.com>
2022-12-31 00:37:56 -0300

    UPDATE se quita id de manga (solo se necesita id de invitacion y estado de aceptacion)

--- src/app/user/invite/inputs/update.input.ts
+++ src/app/user/invite/inputs/update.input.ts
@@ -2,8 +2,8 @@ import { Field, InputType } from '@nestjs/graphql';
 
 @InputType('update_invite')
 export class UpdateInput {
-  @Field(() => Number, { nullable: false })
-  manga_id: number;
+  // @Field(() => Number, { nullable: false })
+  // manga_id: number;
 
   @Field(() => Number, { nullable: false })
   invite_id: number;
