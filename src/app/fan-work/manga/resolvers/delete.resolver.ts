e076ae3cc27069c4a3511d747ecd32fecc3b01b6
Luco97 <renatoluco@gmail.com>
2023-01-01 19:44:17 -0300

    ADD metodo para borrar manga segun id de usuario

--- src/app/fan-work/manga/resolvers/delete.resolver.ts
+++ src/app/fan-work/manga/resolvers/delete.resolver.ts
@@ -1,4 +1,31 @@
-import { Resolver } from '@nestjs/graphql';
+import { UseGuards } from '@nestjs/common';
+import { Args, Context, Mutation, Resolver } from '@nestjs/graphql';
+
+import { AuthGuard } from '@guard/auth.guard';
+import { UpdateOutput } from '../outputs/update.output';
+import { MangaModelService } from '@database/models/manga';
+import { Request } from 'express';
+import { AuthService } from '@shared/auth';
 
 @Resolver()
-export class DeleteResolver {}
+export class DeleteResolver {
+  constructor(
+    private _mangaModel: MangaModelService,
+    private _authService: AuthService,
+  ) {}
+
+  @Mutation(() => UpdateOutput)
+  @UseGuards(AuthGuard)
+  delete(
+    @Args('manga_id') manga_id: number,
+    @Context() context,
+  ): Promise<UpdateOutput> {
+    const req: Request = context.req;
+    const token: string = req.headers?.authorization;
+    const user_id: number = this._authService.userID(token);
+
+    return new Promise<UpdateOutput>((resolve, reject) =>
+      this._mangaModel.find_editable({ user_id, manga_id }),
+    );
+  }
+}
