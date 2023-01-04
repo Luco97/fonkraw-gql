c21705b04ba829a7d33cbfacf0163f96190adf48
Luco97 <renatoluco@gmail.com>
2023-01-04 19:35:26 -0300

    ADD intecreptor con export bonito

--- src/shared/auth/index.ts
+++ src/shared/auth/index.ts
@@ -1,4 +1,6 @@
 export { AuthModule } from './auth.module';
 export { AuthService } from './service/auth.service';
 
+export { UserDataInterceptor } from './interceptors/user-data.interceptor';
+
 export { Payload } from './interface/payload.interface';
