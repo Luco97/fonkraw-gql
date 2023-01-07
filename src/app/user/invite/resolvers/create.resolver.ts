03b82dd7d67b80e6e238f0381684edbf0c817370
Luco97 <renatoluco@gmail.com>
2023-01-06 21:07:30 -0300

    ADD se utiliza interceptor y se quite injeccion de authService

--- src/app/user/invite/resolvers/create.resolver.ts
+++ src/app/user/invite/resolvers/create.resolver.ts
@@ -1,9 +1,9 @@
-import { UseGuards } from '@nestjs/common';
+import { UseGuards, UseInterceptors } from '@nestjs/common';
 import { Args, Context, Mutation, Resolver } from '@nestjs/graphql';
 
 import { Request } from 'express';
 
-import { AuthService } from '@shared/auth';
+import { AuthService, UserDataInterceptor } from '@shared/auth';
 import { AuthGuard } from '@guard/auth.guard';
 import { InviteService } from '../services/invite.service';
 import { CreateInviteInput } from '../inputs/create-invite.input';
@@ -11,28 +11,25 @@ import { CreateInviteOutput } from '../outputs/create-invite.output';
 
 @Resolver()
 export class CreateResolver {
-  constructor(
-    private _authService: AuthService,
-    private _inviteService: InviteService,
-  ) {}
+  constructor(private _inviteService: InviteService) {}
 
   @Mutation(() => CreateInviteOutput)
   @UseGuards(AuthGuard)
+  @UseInterceptors(UserDataInterceptor)
   create_invite(
     @Args('parameters') create_input: CreateInviteInput,
     @Context() context,
   ) {
     const req: Request = context.req;
-    const token: string = req.headers?.authorization;
-    const user_id: number = this._authService.userID(token);
+    const user_id: number = +req.header('user_id');
 
     const { comment, manga_id, to_author_id } = create_input;
 
     return this._inviteService.create_invite({
       comment,
       manga_id,
-      user_id,
       to_author_id,
+      from_author_user_id: user_id,
     });
   }
 }
