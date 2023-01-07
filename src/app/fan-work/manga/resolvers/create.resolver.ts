3e560665923a83876c265c1a456b5c45732903ae
Luco97 <renatoluco@gmail.com>
2023-01-06 21:06:22 -0300

    ADD metodo de resolver usa metodo de servicio

--- src/app/fan-work/manga/resolvers/create.resolver.ts
+++ src/app/fan-work/manga/resolvers/create.resolver.ts
@@ -1,60 +1,36 @@
-import { UseGuards, HttpStatus } from '@nestjs/common';
+import { UseGuards, UseInterceptors } from '@nestjs/common';
 import { Args, Context, Mutation, Resolver } from '@nestjs/graphql';
 
 import { Request } from 'express';
 
-import { AuthService } from '@shared/auth';
 import { AuthGuard } from '@guard/auth.guard';
-import { MangaModelService } from '@database/models/manga';
-import { AuthorModelService } from '@database/models/author';
+import { UserDataInterceptor } from '@shared/auth';
 
 import { CreateInput } from '../inputs/create.input';
 import { CreateOutput } from '../outputs/create.output';
+import { MangaService } from '../services/manga.service';
 
 @Resolver()
 export class CreateResolver {
-  constructor(
-    private _authService: AuthService,
-    private _mangaModel: MangaModelService,
-    private _authorModel: AuthorModelService,
-  ) {}
+  constructor(private _mangaService: MangaService) {}
 
   @Mutation(() => CreateOutput)
   @UseGuards(AuthGuard)
+  @UseInterceptors(UserDataInterceptor)
   create(
     @Args('parameters') createInput: CreateInput,
     @Context() context,
   ): Promise<CreateOutput> {
     const req: Request = context.req;
-    const token: string = req.headers?.authorization;
-
-    const user_id: number = this._authService.userID(token);
+    const user_id: number = +req.header('user_id');
 
     const { cover_url, pages, title } = createInput;
 
-    return new Promise<CreateOutput>((resolve, reject) => {
-      this._authorModel.author_check({ user_id }).then((author) => {
-        if (!author)
-          resolve({
-            status: HttpStatus.OK,
-            message: `you can't create a manga, first create author profile`,
-          });
-        else
-          this._mangaModel
-            .create({
-              title,
-              pages,
-              cover_url,
-              author_id: author.id,
-            })
-            .then((new_manga) =>
-              resolve({
-                manga: new_manga,
-                message: `manga created`,
-                status: HttpStatus.CREATED,
-              }),
-            );
-      });
+    return this._mangaService.create_manga({
+      title,
+      pages,
+      cover_url,
+      creator_user_id: user_id,
     });
   }
 }
