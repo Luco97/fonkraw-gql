ac19119f55dd71f3a1d3c987d4bde8e0da7ea98e
Luco97 <renatoluco@gmail.com>
2023-01-07 23:37:36 -0300

    UPDATE se cambia nombre de output

--- src/app/user/invite/outputs/update.output.ts
+++ src/app/user/invite/outputs/update.output.ts
@@ -1,5 +1,5 @@
 import { extend, ObjectType } from '@nestjs/graphql';
 import { response } from '@utils/response.output';
 
-@ObjectType('update_input_output')
+@ObjectType('update_invite_output')
 export class UpdateOutput extends response {}
