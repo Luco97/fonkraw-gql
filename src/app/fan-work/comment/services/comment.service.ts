67cddf7d51245ce2caeffa5f367c31f7812b58aa
Luco97 <renatoluco@gmail.com>
2023-01-08 19:08:39 -0300

    ADD metodo para buscar todos los comentarios de un manga

--- src/app/fan-work/comment/services/comment.service.ts
+++ src/app/fan-work/comment/services/comment.service.ts
@@ -1,4 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { HttpStatus, Injectable } from '@nestjs/common';
+
+import { ReadAllOutput } from '../outputs/read.output';
+import { CommentModel, CommentModelService } from '@database/models/comment';
 
 @Injectable()
-export class CommentService {}
+export class CommentService {
+  constructor(private _commentModel: CommentModelService) {}
+
+  find_all(parameters: {
+    take?: number;
+    skip?: number;
+    user_id: number;
+    manga_id: number;
+  }): Promise<ReadAllOutput> {
+    const { manga_id, user_id, skip, take } = parameters;
+    return new Promise<ReadAllOutput>((resolve, reject) =>
+      this._commentModel
+        .find_all_from_manga({ manga_id, user_id, skip, take })
+        .then(([comments, count]) =>
+          resolve({
+            count,
+            comments: comments.map<CommentModel>((element) => ({
+              ...element,
+              deletable: element.user.id == user_id,
+            })),
+            status: HttpStatus.OK,
+            message: `total comments: ${count}`,
+          }),
+        ),
+    );
+  }
+}
