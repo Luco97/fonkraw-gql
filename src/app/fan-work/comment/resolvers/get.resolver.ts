f2ece52deb0b6019173d981f42e1ad252323acfc
Luco97 <renatoluco@gmail.com>
2023-01-08 19:08:08 -0300

    UPDATE se lleva codigo para servicio

--- src/app/fan-work/comment/resolvers/get.resolver.ts
+++ src/app/fan-work/comment/resolvers/get.resolver.ts
@@ -1,47 +1,30 @@
-import { HttpStatus } from '@nestjs/common';
+import { UseInterceptors } from '@nestjs/common';
 import { Args, Context, Query, Resolver } from '@nestjs/graphql';
 
 import { Request } from 'express';
 
-import { AuthService } from '@shared/auth';
-import { CommentModel, CommentModelService } from '@database/models/comment';
+import { UserDataInterceptor } from '@shared/auth';
 
 import { ReadAllInput } from '../inputs/read.input';
 import { ReadAllOutput } from '../outputs/read.output';
+import { CommentService } from '../services/comment.service';
 
 @Resolver()
 export class GetResolver {
-  constructor(
-    private _authService: AuthService,
-    private _commentModel: CommentModelService,
-  ) {}
+  constructor(private _commentService: CommentService) {}
 
   @Query(() => ReadAllOutput, { name: 'find_all_comments' })
+  @UseInterceptors(UserDataInterceptor)
   find_all(
     @Args('options', { nullable: false })
     readInput: ReadAllInput,
     @Context() context,
   ) {
     const req: Request = context.req;
-    const token: string = req.headers?.authorization;
-    const user_id: number = this._authService.userID(token);
+    const user_id: number = +req.header('user_id');
 
     const { manga_id, skip, take } = readInput || { skip: 0, take: 5 };
 
-    return new Promise<ReadAllOutput>((resolve, reject) =>
-      this._commentModel
-        .find_all_from_manga({ manga_id, user_id, skip, take })
-        .then(([comments, count]) =>
-          resolve({
-            count,
-            comments: comments.map<CommentModel>((element) => ({
-              ...element,
-              deletable: element.user.id == user_id,
-            })),
-            status: HttpStatus.OK,
-            message: `total comments: ${count}`,
-          }),
-        ),
-    );
+    return this._commentService.find_all({ manga_id, user_id, skip, take });
   }
 }
