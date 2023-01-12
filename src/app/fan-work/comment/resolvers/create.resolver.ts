f2821c0963700c3687cf1d85aa683ee83582dc93
Luco97 <renatoluco@gmail.com>
2023-01-11 21:26:35 -0300

    ADD se utiliza metodo de servicio propio pa comentarios

--- src/app/fan-work/comment/resolvers/create.resolver.ts
+++ src/app/fan-work/comment/resolvers/create.resolver.ts
@@ -1,61 +1,31 @@
-import { HttpStatus, UseGuards } from '@nestjs/common';
+import { UseGuards, UseInterceptors } from '@nestjs/common';
 import { Args, Context, Mutation, Resolver } from '@nestjs/graphql';
 
 import { Request } from 'express';
 
-import { AuthService } from '@shared/auth';
+import { UserDataInterceptor } from '@shared/auth';
 import { AuthGuard } from '@guard/auth.guard';
-import { UserModelService } from '@database/models/user';
-import { MangaModelService } from '@database/models/manga';
-import { CommentModelService } from '@database/models/comment';
 
 import { CreateInput } from '../inputs/create.input';
 import { CreateOutput } from '../outputs/create.output';
+import { CommentService } from '../services/comment.service';
 
 @Resolver()
 export class CreateResolver {
-  constructor(
-    private _authService: AuthService,
-    private _userModel: UserModelService,
-    private _mangaModel: MangaModelService,
-    private _commentModel: CommentModelService,
-  ) {}
+  constructor(private _commentService: CommentService) {}
 
   @Mutation(() => CreateOutput)
   @UseGuards(AuthGuard)
+  @UseInterceptors(UserDataInterceptor)
   create_comment(
     @Args('parameters') createInput: CreateInput,
     @Context() context,
   ) {
     const req: Request = context.req;
-    const token: string = req.headers?.authorization;
-
-    const user_id: number = this._authService.userID(token);
+    const user_id: number = +req.header('user_id');
 
     const { comment, manga_id } = createInput;
 
-    return new Promise<CreateOutput>((resolve, reject) =>
-      this._mangaModel.check_one(manga_id, true).then((count) => {
-        if (!count)
-          resolve({
-            status: HttpStatus.OK,
-            message: `that manga does't exist :(`,
-          });
-        else
-          Promise.all([
-            this._commentModel.create({ comment, manga_id, user_id }),
-            this._userModel.find_one_by_id(user_id),
-          ]).then(([new_comment, user]) =>
-            resolve({
-              status: HttpStatus.CREATED,
-              message: `comment created`,
-              comment: {
-                ...new_comment,
-                user,
-              },
-            }),
-          );
-      }),
-    );
+    return this._commentService.create({ comment, manga_id, user_id });
   }
 }
