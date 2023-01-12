5b0733d9db6d09ec64e9e79c808fa8f8fdb8f106
Luco97 <renatoluco@gmail.com>
2023-01-11 21:26:04 -0300

    ADD se agrega metodo create y se quiten algunos elementos pa prueba

--- src/app/fan-work/comment/services/comment.service.ts
+++ src/app/fan-work/comment/services/comment.service.ts
@@ -1,11 +1,16 @@
 import { HttpStatus, Injectable } from '@nestjs/common';
 
 import { ReadAllOutput } from '../outputs/read.output';
+import { CreateOutput } from '../outputs/create.output';
+import { MangaModelService } from '@database/models/manga';
 import { CommentModel, CommentModelService } from '@database/models/comment';
 
 @Injectable()
 export class CommentService {
-  constructor(private _commentModel: CommentModelService) {}
+  constructor(
+    private _mangaModel: MangaModelService,
+    private _commentModel: CommentModelService,
+  ) {}
 
   find_all(parameters: {
     take?: number;
@@ -28,6 +33,30 @@ export class CommentService {
             message: `total comments: ${count}`,
           }),
         ),
+    );
+  }
+
+  create(parameters: { comment: string; manga_id: number; user_id: number }) {
+    const { comment, manga_id, user_id } = parameters;
+
+    return new Promise<CreateOutput>((resolve, reject) =>
+      this._mangaModel.check_one(manga_id, true).then((count) => {
+        if (!count)
+          resolve({
+            status: HttpStatus.OK,
+            message: `that manga does't exist :(`,
+          });
+        else
+          this._commentModel
+            .create({ comment, manga_id, user_id })
+            .then((new_comment) =>
+              resolve({
+                status: HttpStatus.CREATED,
+                message: `comment created`,
+                comment: new_comment,
+              }),
+            );
+      }),
     );
   }
 }
