ce9b0d4f81ca87d1c521d2b1a11df3f313a2dfc7
Luco97 <renatoluco@gmail.com>
2023-01-12 19:26:52 -0300

    ADD se agrega servicio en modulo

--- src/app/fan-work/author/author.module.ts
+++ src/app/fan-work/author/author.module.ts
@@ -1,15 +1,20 @@
 import { Module } from '@nestjs/common';
 
+// Modules
 import { AuthModule } from '@shared/auth';
 import { UserModelModule } from '@database/models/user';
 import { AuthorModelModule } from '@database/models/author';
 
+// Resolvers
 import { ReadResolver } from './resolvers/read.resolver';
 import { CreateResolver } from './resolvers/create.resolver';
 import { UpdateResolver } from './resolvers/update.resolver';
 
+// Services
+import { AuthorService } from './services/author.service';
+
 @Module({
   imports: [AuthorModelModule, UserModelModule, AuthModule],
-  providers: [CreateResolver, UpdateResolver, ReadResolver],
+  providers: [CreateResolver, UpdateResolver, ReadResolver, AuthorService],
 })
 export class AuthorModule {}
