b187d71a701077baadfa0c72777973794ec5c596
Luco97 <renatoluco@gmail.com>
2023-01-12 19:25:52 -0300

    ADD se utiliza metoodo de servicio para dejar resolver limpio

--- src/app/fan-work/comment/resolvers/delete.resolver.ts
+++ src/app/fan-work/comment/resolvers/delete.resolver.ts
@@ -1,46 +1,27 @@
-import { UseGuards, HttpStatus } from '@nestjs/common';
+import { UseGuards, UseInterceptors } from '@nestjs/common';
 import { Args, Context, Mutation, Resolver } from '@nestjs/graphql';
 
 import { Request } from 'express';
 
-import { AuthService } from '@shared/auth';
+import { UserDataInterceptor } from '@shared/auth';
 import { AuthGuard } from '@guard/auth.guard';
-import { CommentModelService } from '@database/models/comment';
 
-import { DeleteInput } from '../inputs/delete.input';
 import { DeleteOutput } from '../outputs/delete.output';
+import { CommentService } from '../services/comment.service';
 @Resolver()
 export class DeleteResolver {
-  constructor(
-    private _authService: AuthService,
-    private _commentModel: CommentModelService,
-  ) {}
+  constructor(private _commentService: CommentService) {}
 
   @Mutation(() => DeleteOutput)
   @UseGuards(AuthGuard)
+  @UseInterceptors(UserDataInterceptor)
   delete_comment(
-    @Args('parameters') deleteInput: DeleteInput,
+    @Args('parameters', { nullable: false }) comment_id: number,
     @Context() context,
   ) {
     const req: Request = context.req;
-    const token: string = req.headers?.authorization;
-    const user_id: number = this._authService.userID(token);
+    const user_id: number = +req.header('user_id');
 
-    const { comment_id, user_comment_id } = deleteInput;
-
-    return new Promise<DeleteOutput>((resolve, reject) => {
-      if (user_id != user_comment_id)
-        resolve({
-          message: `you can't delete this comment`,
-          status: HttpStatus.UNAUTHORIZED,
-        });
-      else
-        this._commentModel.delete(comment_id).then(() =>
-          resolve({
-            status: HttpStatus.OK,
-            message: `comment with id: ${comment_id} deleted`,
-          }),
-        );
-    });
+    return this._commentService.delete({ comment_id, user_id });
   }
 }
