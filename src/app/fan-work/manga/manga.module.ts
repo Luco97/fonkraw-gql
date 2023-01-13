19bf49eb123f1f7cf9e67af94b24c69d3d7a8f11
Luco97 <renatoluco@gmail.com>
2023-01-13 17:31:56 -0300

    ADD se agrega modulo para usar modelo de usuario

--- src/app/fan-work/manga/manga.module.ts
+++ src/app/fan-work/manga/manga.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 
 // Modules
 import { AuthModule } from '@shared/auth';
+import { UserModelModule } from '@database/models/user';
 import { MangaModelModule } from '@database/models/manga';
 import { GenreModelModule } from '@database/models/genre';
 import { AuthorModelModule } from '@database/models/author';
@@ -16,7 +17,13 @@ import { UpdateResolver } from './resolvers/update.resolver';
 import { MangaService } from './services/manga.service';
 
 @Module({
-  imports: [AuthModule, MangaModelModule, AuthorModelModule, GenreModelModule],
+  imports: [
+    AuthModule,
+    UserModelModule,
+    MangaModelModule,
+    GenreModelModule,
+    AuthorModelModule,
+  ],
   providers: [
     // Resolvers
     ReadResolver,
