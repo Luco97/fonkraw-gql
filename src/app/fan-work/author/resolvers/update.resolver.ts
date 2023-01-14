d38d2afd165ffb24900d0e1e82785f30f6fe67cd
Luco97 <renatoluco@gmail.com>
2023-01-14 19:39:06 -0300

    ADD se utiliza metodo de servicio para dejar resolver de update con menor codigo

--- src/app/fan-work/author/resolvers/update.resolver.ts
+++ src/app/fan-work/author/resolvers/update.resolver.ts
@@ -1,52 +1,32 @@
-import { UseGuards, HttpStatus } from '@nestjs/common';
+import { UseGuards, UseInterceptors } from '@nestjs/common';
 import { Args, Context, Mutation, Resolver } from '@nestjs/graphql';
 
 import { Request } from 'express';
 
-import { AuthService } from '@shared/auth';
 import { AuthGuard } from '@guard/auth.guard';
 import { response } from '@utils/response.output';
-import { UserModelService } from '@database/models/user';
-import { AuthorModelService } from '@database/models/author';
+import { UserDataInterceptor } from '@shared/auth';
 import { UpdateInput } from '../inputs/update.input';
+import { AuthorService } from '../services/author.service';
 
 @Resolver()
 export class UpdateResolver {
-  constructor(
-    private _authService: AuthService,
-    private _userModel: UserModelService,
-    private _authorModel: AuthorModelService,
-  ) {}
+  constructor(private _authorService: AuthorService) {}
 
   @Mutation(() => response)
   @UseGuards(AuthGuard)
+  @UseInterceptors(UserDataInterceptor)
   update(
     @Args('author', { nullable: true }) updateInput: UpdateInput,
     @Context() context,
   ): Promise<response> {
     const req: Request = context.req;
-    const token: string = req.headers?.authorization;
+    // const token: string = req.headers?.authorization;
 
     const { description } = updateInput;
-    const user_id: number = this._authService.userID(token);
+    // const user_id: number = this._authService.userID(token);
+    const user_id: number = +req.header('user_id');
 
-    return new Promise<response>((resolve, reject) => {
-      this._authorModel.author_check({ user_id }).then((author) => {
-        if (!author)
-          resolve({
-            message: 'author not found',
-            status: HttpStatus.NOT_FOUND,
-          });
-        else
-          this._authorModel
-            .update({ author_id: author.id, description })
-            .then(() =>
-              resolve({
-                message: 'description updated',
-                status: HttpStatus.ACCEPTED,
-              }),
-            );
-      });
-    });
+    return this._authorService.update_info({ description, user_id });
   }
 }
