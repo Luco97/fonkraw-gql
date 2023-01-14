7f98146371c767630c944ec549a8412124c6e0c1
Luco97 <renatoluco@gmail.com>
2023-01-14 19:38:33 -0300

    ADD metodo para update de descripcion de autor

--- src/app/fan-work/author/services/author.service.ts
+++ src/app/fan-work/author/services/author.service.ts
@@ -1,4 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpStatus } from '@nestjs/common';
+
+import { response } from '@utils/response.output';
+import { AuthorModelService } from '@database/models/author';
 
 @Injectable()
-export class AuthorService {}
+export class AuthorService {
+  constructor(private _authorModel: AuthorModelService) {}
+
+  update_info(parameters: { description: string; user_id: number }) {
+    const { description, user_id } = parameters;
+
+    return new Promise<response>((resolve, reject) => {
+      this._authorModel.author_check({ user_id }).then((author) => {
+        if (!author)
+          resolve({
+            message: 'author not found',
+            status: HttpStatus.NOT_FOUND,
+          });
+        else
+          this._authorModel
+            .update({ author_id: author.id, description })
+            .then(() =>
+              resolve({
+                message: 'description updated',
+                status: HttpStatus.ACCEPTED,
+              }),
+            );
+      });
+    });
+  }
+}
