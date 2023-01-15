ea9248ca51e984269d03a9c5440b69c896a5b461
Luco97 <renatoluco@gmail.com>
2023-01-15 01:02:02 -0300

    ADD se utiliza servicio para dejar resolver limpio

--- src/app/fan-work/author/resolvers/create.resolver.ts
+++ src/app/fan-work/author/resolvers/create.resolver.ts
@@ -1,54 +1,30 @@
-import { UseGuards, HttpStatus } from '@nestjs/common';
+import { UseGuards, UseInterceptors } from '@nestjs/common';
 import { Args, Context, Mutation, Resolver } from '@nestjs/graphql';
 
 import { Request } from 'express';
 
-import { AuthService } from '@shared/auth';
+import { UserDataInterceptor } from '@shared/auth';
 import { CreateInput } from '../inputs/create.input';
-import { UserModelService } from '@database/models/user';
-import { AuthorModel, AuthorModelService } from '@database/models/author';
 import { AuthGuard } from '@guard/auth.guard';
 import { CreateOutput } from '../outputs/create.output';
+import { AuthorService } from '../services/author.service';
 
 @Resolver()
 export class CreateResolver {
-  constructor(
-    private _authService: AuthService,
-    private _userModel: UserModelService,
-    private _authorModel: AuthorModelService,
-  ) {}
+  constructor(private _authorService: AuthorService) {}
 
   @Mutation(() => CreateOutput)
   @UseGuards(AuthGuard)
+  @UseInterceptors(UserDataInterceptor)
   create(
     @Args('author', { nullable: true }) createInput: CreateInput,
     @Context() context,
   ): Promise<CreateOutput> {
     const req: Request = context.req;
-    const token: string = req.headers?.authorization;
-    
-    const { alias, description } = createInput;
-    const user_id: number = this._authService.userID(token);
+    const user_id: number = +req.header('user_id');
 
-    return new Promise<CreateOutput>((resolve, reject) => {
-      this._authorModel.author_check({ user_id, alias }).then((author) => {
-        if (author)
-          resolve({
-            author,
-            message: 'author already exist',
-            status: HttpStatus.AMBIGUOUS,
-          });
-        else
-          this._authorModel
-            .create({ alias, description, user_id })
-            .then((new_author) =>
-              resolve({
-                author: new_author,
-                message: `Happy to see you, ${new_author.alias}`,
-                status: HttpStatus.OK,
-              }),
-            );
-      });
-    });
+    const { alias, description } = createInput;
+
+    return this._authorService.create({ alias, description, user_id });
   }
 }
