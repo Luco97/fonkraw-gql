f10baf6c9b4097498ac015f8a1ecbae902ad9090
Luco97 <renatoluco@gmail.com>
2023-01-15 01:00:57 -0300

    ADD metodo para crear author

--- src/app/fan-work/author/services/author.service.ts
+++ src/app/fan-work/author/services/author.service.ts
@@ -2,12 +2,45 @@ import { Injectable, HttpStatus } from '@nestjs/common';
 
 import { response } from '@utils/response.output';
 import { AuthorModelService } from '@database/models/author';
+import { CreateOutput } from '../outputs/create.output';
 
 @Injectable()
 export class AuthorService {
   constructor(private _authorModel: AuthorModelService) {}
 
-  update_info(parameters: { description: string; user_id: number }) {
+  create(parameters: {
+    alias: string;
+    description: string;
+    user_id: number;
+  }): Promise<CreateOutput> {
+    const { alias, description, user_id } = parameters;
+
+    return new Promise<CreateOutput>((resolve, reject) => {
+      this._authorModel.author_check({ user_id, alias }).then((author) => {
+        if (author)
+          resolve({
+            author,
+            message: 'author already exist',
+            status: HttpStatus.AMBIGUOUS,
+          });
+        else
+          this._authorModel
+            .create({ alias, description, user_id })
+            .then((new_author) =>
+              resolve({
+                author: new_author,
+                message: `Happy to see you, ${new_author.alias}`,
+                status: HttpStatus.OK,
+              }),
+            );
+      });
+    });
+  }
+
+  update_info(parameters: {
+    description: string;
+    user_id: number;
+  }): Promise<response> {
     const { description, user_id } = parameters;
 
     return new Promise<response>((resolve, reject) => {
