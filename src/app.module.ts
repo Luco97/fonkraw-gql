1418d973a901d9abcf5dcfebf389d8866fba0fde
Luco97 <renatoluco@gmail.com>
2023-01-17 20:24:38 -0300

    UPDATE version 1.0.0 de esta porqueria

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
+import { AuthModule } from '@shared/auth';
+import { FanWorkModule } from './app/fan-work.module';
+import { RoleModelModule } from '@database/models/role';
+// import { MailModule } from '@shared/mail';
+// import { DatabaseModule } from './database/database.module';
+// import { AuthorModelModule } from './database/models/author/author-model.module';
+// import { GenreModelModule } from './database/models/genre/genre-model.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    // DatabaseModule,
+    AuthModule,
+    FanWorkModule,
+    RoleModelModule,
+    // MailModule,
+    // AuthorModelModule,
+    // GenreModelModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
