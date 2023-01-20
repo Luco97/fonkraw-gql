72f240b4b9e7d10a3de8a584e2fc8803f83ad158
Luco97 <renatoluco@gmail.com>
2023-01-19 22:21:02 -0300

    ADD export bonito para template de invitacion

--- src/shared/mail/index.ts
+++ src/shared/mail/index.ts
@@ -1,3 +1,6 @@
 export { MailModule } from './mail.module';
 export { MailService } from './service/mail.service';
-export { html_template_verify } from './template/verify.template';
+export {
+  html_template_verify,
+  html_template_invite,
+} from './template/verify.template';
