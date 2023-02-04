ffad467535dffaa95691781cd55063227686dec2
Luco97 <renatoluco@gmail.com>
2023-02-04 18:26:05 -0300

    ADD metodo para actualizar descripcion de un genero

--- src/app/fan-work/genre/service/genre.service.ts
+++ src/app/fan-work/genre/service/genre.service.ts
@@ -1,6 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { GenreModelService } from '@database/models/genre';
+import { HttpStatus, Injectable } from '@nestjs/common';
+import { UpdateOutput } from '../outputs/update.output';
 
 @Injectable()
 export class GenreService {
-  constructor() {}
+  constructor(private _genreModel: GenreModelService) {}
+
+  update_description(parameters: {
+    genre_id: number;
+    new_description: string;
+  }): Promise<UpdateOutput> {
+    const { genre_id, new_description } = parameters;
+
+    return new Promise<UpdateOutput>((resolve, reject) =>
+      this._genreModel.find_one(genre_id).then((genre) => {
+        if (!genre)
+          resolve({
+            message: `genre with id = ${genre_id} not exist`,
+            status: HttpStatus.OK,
+          });
+        else
+          this._genreModel.update(genre_id, new_description).then(() =>
+            resolve({
+              message: `genre with id = ${genre_id} update`,
+              status: HttpStatus.OK,
+            }),
+          );
+      }),
+    );
+  }
 }
