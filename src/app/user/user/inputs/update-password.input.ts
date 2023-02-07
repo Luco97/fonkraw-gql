fae2a9940e05368153bcc3007d3ec9941bb8907d
Luco97 <renatoluco@gmail.com>
2023-02-06 21:06:31 -0300

    UPDATE se le definen nombres para inputs de update password en usuario

--- src/app/user/user/inputs/update-password.input.ts
+++ src/app/user/user/inputs/update-password.input.ts
@@ -1,7 +1,7 @@
 import { Field, InputType } from '@nestjs/graphql';
 import { MinLength } from 'class-validator';
 
-@InputType()
+@InputType('update_password_input')
 export class UpdatePasswordInput {
   @Field(() => String, { description: 'password for the user' })
   @MinLength(7)
