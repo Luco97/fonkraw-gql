94b95fd6157fc500148459257d02f46bb6170c21
Luco97 <renatoluco@gmail.com>
2023-02-07 19:58:19 -0300

    ADD noombre para outputs en resolver de comentarios

--- src/app/fan-work/comment/outputs/create.output.ts
+++ src/app/fan-work/comment/outputs/create.output.ts
@@ -2,7 +2,7 @@ import { CommentModel } from '@database/models/comment';
 import { Field, ObjectType } from '@nestjs/graphql';
 import { response } from '@utils/response.output';
 
-@ObjectType()
+@ObjectType('create_comment_output')
 export class CreateOutput extends response {
   @Field(() => CommentModel, { nullable: true })
   comment?: CommentModel;
--- src/app/fan-work/comment/outputs/delete.output.ts
+++ src/app/fan-work/comment/outputs/delete.output.ts
@@ -1,5 +1,5 @@
 import { ObjectType } from '@nestjs/graphql';
 import { response } from '@utils/response.output';
 
-@ObjectType()
+@ObjectType('delete_comment_output')
 export class DeleteOutput extends response {}
