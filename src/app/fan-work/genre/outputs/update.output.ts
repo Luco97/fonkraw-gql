f16c26c6fd1b08a49016a550458533ec2ed26e0c
Luco97 <renatoluco@gmail.com>
2023-02-08 21:53:00 -0300

    ADD nombre para output de actualizar genero

--- src/app/fan-work/genre/outputs/update.output.ts
+++ src/app/fan-work/genre/outputs/update.output.ts
@@ -1,5 +1,5 @@
 import { ObjectType } from '@nestjs/graphql';
 import { response } from '@utils/response.output';
 
-@ObjectType()
+@ObjectType('update_genre_output')
 export class UpdateOutput extends response {}
