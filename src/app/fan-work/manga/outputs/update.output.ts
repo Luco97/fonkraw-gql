7981ad8920f6b0f0d99e3152e1d10e04964840a2
Luco97 <renatoluco@gmail.com>
2023-02-08 21:53:19 -0300

    ADD nombre para output de actualizar manga

--- src/app/fan-work/manga/outputs/update.output.ts
+++ src/app/fan-work/manga/outputs/update.output.ts
@@ -1,5 +1,5 @@
 import { ObjectType } from '@nestjs/graphql';
 import { response } from '@utils/response.output';
 
-@ObjectType('update_genre')
+@ObjectType('update_manga')
 export class UpdateOutput extends response {}
