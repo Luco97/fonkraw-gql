0ce60c6a8b9af569fd34ff819808e2c227ad849e
Luco97 <renatoluco@gmail.com>
2023-02-08 21:51:35 -0300

    ADD nombre a output para actualizar autor

--- src/app/fan-work/author/outputs/update.output.ts
+++ src/app/fan-work/author/outputs/update.output.ts
@@ -3,7 +3,7 @@ import { Field, ObjectType } from '@nestjs/graphql';
 import { response } from '@utils/response.output';
 import { AuthorModel } from '@database/models/author';
 
-@ObjectType()
+@ObjectType('update_author_output')
 export class UpdateOutput extends response {
   @Field(() => AuthorModel)
   author: AuthorModel;
