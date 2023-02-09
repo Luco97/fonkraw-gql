db04fe548af42c5315e7f075b8a2f89f49e332d9
Luco97 <renatoluco@gmail.com>
2023-02-08 21:52:29 -0300

    ADD nombre para output de obtener comentarios

--- src/app/fan-work/comment/outputs/read.output.ts
+++ src/app/fan-work/comment/outputs/read.output.ts
@@ -3,7 +3,7 @@ import { Field, ObjectType } from '@nestjs/graphql';
 import { response } from '@utils/response.output';
 import { CommentModel } from '@database/models/comment';
 
-@ObjectType()
+@ObjectType('read_all_comment_output')
 export class ReadAllOutput extends response {
   @Field(() => [CommentModel])
   comments: CommentModel[];
