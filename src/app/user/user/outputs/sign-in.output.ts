1cd91d6ff4734bc20caf8d5dacfa03351679945d
Luco97 <renatoluco@gmail.com>
2023-02-09 21:54:07 -0300

    ADD output para ingreso de usuario

--- src/app/user/user/outputs/sign-in.output.ts
+++ src/app/user/user/outputs/sign-in.output.ts
@@ -1,7 +1,7 @@
 import { Field, ObjectType } from '@nestjs/graphql';
 import { response } from '@utils/response.output';
 
-@ObjectType({ description: 'output object in sign in mutation' })
+@ObjectType('sign_output', { description: 'output object in sign in mutation' })
 export class SignInOutput extends response {
   @Field(() => String, { nullable: true, defaultValue: '' })
   token?: string;
