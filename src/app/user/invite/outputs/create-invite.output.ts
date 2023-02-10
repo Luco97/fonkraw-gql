539f95d32af89874dc8ebb98d78218771823eaac
Luco97 <renatoluco@gmail.com>
2023-02-09 21:53:09 -0300

    ADD nombre para output de crear invitacion

--- src/app/user/invite/outputs/create-invite.output.ts
+++ src/app/user/invite/outputs/create-invite.output.ts
@@ -3,7 +3,7 @@ import { Field, ObjectType } from '@nestjs/graphql';
 import { response } from '@utils/response.output';
 import { InviteModel } from '@database/models/invite';
 
-@ObjectType()
+@ObjectType('create_invite_output')
 export class CreateInviteOutput extends response {
   @Field(() => InviteModel, { nullable: true })
   invite?: InviteModel;
