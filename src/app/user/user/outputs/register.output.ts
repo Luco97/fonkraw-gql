d17088f4b2eb4758466b3e1083c05fb704948266
Luco97 <renatoluco@gmail.com>
2023-02-11 00:09:14 -0300

    ADD nombre para output de registro

--- src/app/user/user/outputs/register.output.ts
+++ src/app/user/user/outputs/register.output.ts
@@ -1,5 +1,7 @@
 import { ObjectType } from '@nestjs/graphql';
 import { response } from '@utils/response.output';
 
-@ObjectType({ description: 'output object of register_user' })
+@ObjectType('register_output', {
+  description: 'output object of register_user',
+})
 export class RegisterOutput extends response {}
